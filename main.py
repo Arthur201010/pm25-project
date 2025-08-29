f1b57814f0ed6f92352f94512f6c6cb608e35407
Arthur210101 <artuur201010@gmail.com>
2025-08-29 22:02:07 +0800

    08.29:

---
+++ main.py
@@ -0,0 +1,84 @@
+from flask import Flask, render_template,Response
+from datetime import datetime
+
+from pm25 import get_from_mysql,write_data_to_mysql,get_avg_from_mysql
+import json
+
+books = {
+    1: {
+        "name": "Python book",
+        "price": 299,
+        "image_url": "https://im2.book.com.tw/image/getImage?i=https://www.books.com.tw/img/CN1/136/11/CN11361197.jpg&v=58096f9ck&w=348&h=348",
+    },
+    2: {
+        "name": "Java book",
+        "price": 399,
+        "image_url": "https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/087/31/0010873110.jpg&v=5f7c475bk&w=348&h=348",
+    },
+    3: {
+        "name": "C# book",
+        "price": 499,
+        "image_url": "https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/036/04/0010360466.jpg&v=62d695bak&w=348&h=348",
+    },
+}  # 8/22 2:10:00
+
+app = Flask(__name__)
+
+@app.route("/avg-pm25")
+def get_avg_pm25():
+    result= get_avg_from_mysql()
+    print(result)
+    county=[r[0] for r in result]
+    pm25=[float(r[1]) for r in result]
+
+    return Response( 
+        json.dumps({"county":county,"pm25":pm25}, ensure_ascii=False),
+        mimetype="application/json",
+    )
+@app.route("/update-db")
+def update_db():
+    result=write_data_to_mysql()
+
+    return json.dumps(result, ensure_ascii=False)
+
+@app.route("/pm25")
+def get_pm25():
+    print("雲端資料下載.....")
+    values = get_from_mysql()
+    # print(values)
+    columns = ["站點名稱", "縣市", "PM2.5", "更新時間", "單位"]
+    return render_template("pm25.html", columns=columns, values=values)
+
+
+@app.route("/bmi/height=<h>&weight=<w>")
+def get_bmi(h, w):
+    bmi = round(eval(w) / (eval(h) / 100) ** 2, 2)
+    return f"<h1>身高:{h} 體重:{w} <br>BMI:{bmi}</h1>"
+
+
+@app.route("/books")
+@app.route("/books/id=<int:id>")
+def get_books(id=None):  # 8/22 1:00:00
+    try:
+        # books = {1: "Python book", 2: "Java book", 3: "Flask book"}
+        if id == None:
+            return render_template("books.html", books=books)
+        return books[id]
+    except Exception as e:
+        return f"編號錯誤:{e}"
+
+
+@app.route("/nowtime")
+def now_time():
+    time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(time)
+    return time
+
+
+@app.route("/")
+def index():
+    time = now_time()
+    return render_template("index.html", t=time, name="Hans")
+
+
+app.run(debug=True)
---
+++ pm25.py
@@ -0,0 +1,145 @@
+import pymysql
+import requests
+
+header = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36"
+}
+# 8/20 0:30:00
+
+table_str = """ 
+create table if not exists pm25(
+id int auto_increment primary key,
+site varchar(25),
+county varchar(50),
+pm25 int,
+datacreationdate datetime,
+itemunit varchar(20),
+unique key site_time(site,datacreationdate)
+)
+"""
+sqlstr = "insert ignore into pm25(site,county,pm25,datacreationdate,itemunit)\
+      values(%s,%s,%s,%s,%s)"
+
+url = "https://data.moenv.gov.tw/api/v2/aqx_p_02?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=datacreationdate%20desc&format=JSON"
+conn, cursor = None, None
+
+
+def open_db():
+    global conn, cursor
+    try:
+        # conn = pymysql.connect(
+        #     host="localhost", user="root", passwd="", port=3307, database="demo"
+        # )
+        conn = pymysql.connect(
+            host="mysql-c87907a-arthur201010-4fba.j.aivencloud.com",
+            user="avnadmin",
+            password="AVNS_HWzrTchr2PZUlSOko5r",
+            port=11977,
+            database="defaultdb",
+        )
+        # print(conn)
+        cursor = conn.cursor()
+        # cursor.execute(table_str)
+        # conn.commit()
+        print("資料庫開啟成功!")
+    except Exception as e:
+        print(e)
+
+
+def close_db():
+    if conn is not None:
+        conn.close()
+        print("資料庫關閉結束!")
+
+
+def get_opendata():  # 8/20 2:35:00
+    values = None
+    try:
+        resp = requests.get(url, verify=False)
+        if resp.status_code == 200:
+            print("status_code=", resp.status_code)
+            datas = resp.json()["records"]
+            values = [
+                list(data.values()) for data in datas if list(data.values())[2] != ""
+            ]
+        else:
+            print(resp.status_code, "取得內容失敗")
+    except Exception as e:
+        print("錯誤", e)
+    return values
+
+
+def write_sql():
+    try:
+        values = get_opendata()
+        if len(values) == 0:
+            print("目前無資料")
+            return
+        size = cursor.executemany(sqlstr, values)
+        conn.commit()
+        print(f"寫入{size}筆資料成功")
+        return size
+    except Exception as e:
+        print(e)
+    return 0
+
+def write_data_to_mysql():
+    try:
+        open_db()
+        size=write_sql()
+
+        return{"結果":"success","筆數":size}
+    except Exception as e:
+        print(e)
+        return {"結果":"failure","message":str(e)}
+    finally:
+        close_db
+
+def get_avg_from_mysql():  # 8/27 2:18:00  2:45:00
+    try:
+        open_db()
+        sqlstr="""
+        select county,round(avg(pm25),2) from pm25 group by county;
+        """
+        cursor.execute(sqlstr)
+        datas=cursor.fetchall()
+    
+        return datas
+    except Exception as e:
+        print("雲端資料庫擷取失敗", e)
+    finally:
+        close_db()
+    return None
+
+
+def get_from_mysql():  # 8/27 2:18:00  2:45:00
+    try:
+        open_db()
+        sqlstr = "select max(datacreationdate) from pm25"
+        cursor.execute(sqlstr)
+        max_data = cursor.fetchone()
+        print("最近時間:", max_data)
+
+        sqlstr = (
+            "select site,county,pm25,datacreationdate,"
+            "itemunit from pm25 "
+            "where datacreationdate=(select max(datacreationdate) from pm25);"
+        )
+        cursor.execute(sqlstr)
+        # cursor.execute(sqlstr, (max_data,))
+        datas = cursor.fetchall()
+        # 去掉 id 當使用 (select * from pm25)時
+        # datas = [data[1:0] for data in datas]
+
+        return datas
+    except Exception as e:
+        print("雲端資料庫擷取失敗", e)
+    finally:
+        close_db()
+    return None
+
+if __name__=="__main__":
+# print(get_from_mysql())
+# write_data_to_mysql()
+# print(get_opendata())
+    print(get_avg_from_mysql())
